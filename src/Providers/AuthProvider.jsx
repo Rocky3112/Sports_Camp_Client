ce82b9947c219bb292b9f3bdbe0bf27c5ada4440
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-07-27 15:53:00 +0600

    created auth provider

---
+++ src/Providers/AuthProvider.jsx
@@ -0,0 +1,45 @@
+/* eslint-disable react/prop-types */
+
+import { createContext, useState } from "react";
+import { app } from "../firebase/firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  const authInfo = {
+    user,
+    createUser,
+    loading,
+    signIn,
+    logOut,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,16 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { router } from './Routes/Routes.jsx';
+import AuthProvider from './Providers/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    
+   <AuthProvider>
    <div className=' max-w-screen-xl mx-auto'>
    <RouterProvider router={router} />
    </div>
+   </AuthProvider>
     
   </React.StrictMode>,
 )
