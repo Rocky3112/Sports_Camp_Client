53e5a9f64c6c3e081abb05bebf7f17d930721d77
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-07-27 16:44:02 +0600

    sign Up page done with react hook form & opretaion

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/SignUp/SignUp";
 
 export const router = createBrowserRouter([
     {
@@ -16,6 +17,10 @@ export const router = createBrowserRouter([
           path: 'login' ,
           element: <Login></Login> 
         },
+        {
+          path: "signup",
+          element:<SignUp></SignUp>
+      },
       ]
     }
   ])
