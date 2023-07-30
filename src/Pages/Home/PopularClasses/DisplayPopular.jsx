8756a8439e2ebee8e44ce1e8514fda07dadd7c80
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-07-30 20:19:53 +0600

    popular section displayed

---
+++ src/Pages/Home/PopularClasses/DisplayPopular.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/prop-types */
+
+const DisplayPopular = ({ item }) => {
+  console.log(item);
+  const { name, image, phone, email, details } = item;
+  return (
+    <div className="">
+      <div className="card card-compact w-96 bg-base-100 shadow-xl  h-[420px] p-4">
+        <figure>
+          <img className="rounded-xl"
+            src={image}
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{name}</h2>
+          <p>{details}</p>
+          <h2>{email}</h2>
+          <h2>{phone}</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DisplayPopular;
--- src/Pages/Home/PopularClasses/PopularClasses.jsx
+++ src/Pages/Home/PopularClasses/PopularClasses.jsx
@@ -1,10 +1,16 @@
+import { Link } from "react-router-dom";
 import useClasses from "../../../Hooks/useClasses";
 import PartTitle from "../../../components/PartTitle/PartTitle";
+import DisplayPopular from "./DisplayPopular";
+
 
 const PopularClasses = () => {
 
     const [classes] = useClasses()
     console.log(classes);
+
+   
+    const popular = classes.filter(item=>item.category=== 'popular')
     return (
         <div>
             <section className="my-10">
@@ -13,6 +19,20 @@ const PopularClasses = () => {
         ></PartTitle>
 
         </section>
+
+        <div className=" grid md:grid-cols-3 gap-4">
+            {
+                popular.map(item =><DisplayPopular
+                key ={item._id}
+                item ={item}
+                ></DisplayPopular>)
+            }
+        </div>
+        <div className="text-center">
+        <Link to ='/classes'>
+        <button  className="uppercase btn btn-outline my-3 border-0 border-b-2  mx-auto">View All Classes</button>
+        </Link>
+        </div>
         </div>
     );
 };
