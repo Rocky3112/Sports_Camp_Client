24551f772f2e22e58631d56c3a16547637bded7b
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-09-05 01:05:10 +0600

    update entire contact section

--- src/Pages/Home/Contact/Contact.jsx
+++ src/Pages/Home/Contact/Contact.jsx
@@ -1,71 +1,105 @@
-import { Fade } from "react-awesome-reveal";
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable no-unused-vars */
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
+import { ToastContainer, toast } from 'react-toastify';
+import { Fade, Zoom } from 'react-awesome-reveal';
+import Lottie from "lottie-react";
+import contact from '../../../../public/contact.json'
 
 const Contact = () => {
+    const form = useRef();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        emailjs.sendForm(`service_ubx0k4e`, `template_tv5gkjj`, form.current, `fwUBPkDRGJGX8491J`)
+            .then(function (response) {
+                toast.success('thanks for your message');
+                e.target.reset();
+            }, function (error) {
+                
+            });
+    }
     return (
-        <div className='my-12 bg-base-200 p-4 lg:p-12'>
-            <h1 className="text-3xl font-bold text-center my-3">Contact</h1>
-            <div className='flex flex-col md:flex-row justify-center gap-8 lg:gap-28' >
-                <Fade direction="left" duration={1500}>
-                <div className='card w-80 md:w-96 p-4  hover:bg-base-300'>
-                    <div className=''>
-                        <h2 className="text-2xl">Phone</h2>
-                        <figure style={{ justifyContent: 'flex-start' }} className='flex items-center' >
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-18 w-12" viewBox="0 0 20 20" fill="currentColor">
-                                <path fillRule="evenodd" d="M7 2a2 2 0 00-2 2v12a2 2 0 002 2h6a2 2 0 002-2V4a2 2 0 00-2-2H7zm3 14a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
-                            </svg>
-                            <a href="tel:+8801772893112">+8801772893112</a>
-                        </figure>
-                    </div>
-                    <div className='my-5'>
-                        <h2 className="text-2xl">Email</h2>
-                        <figure style={{ justifyContent: 'flex-start' }} className='flex items-center' >
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-18 w-12" viewBox="0 0 20 20" fill="currentColor">
-                                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-                            </svg>
-                            <a href="mailto:support@powertools.com">support@powertools.com</a>
-                        </figure>
-                    </div>
-                    <div>
-                        <h2 className="text-2xl">Address</h2>
-                        <figure style={{ justifyContent: 'flex-start' }} className='flex items-center' >
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-18 w-12" viewBox="0 0 20 20" fill="currentColor">
-                                <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
-                            </svg>
-                            <address>Kajla, Matihar, Rajshahi </address>
-                        </figure>
-                    </div>
+        <div id='contactMe' className='mt-20 mb-10'>
+            <div className=" min-h-screen">
+                <div>
+                   <h3 className="text-4xl font-semibold text-center text-yellow-500 py-3">Contact Info</h3>
+                    <h3 className='text-2xl mx-20'>
+                    Let's Connect! Reach out through the Contact Me section to discuss your web development needs, collaborations, or any inquiries. I'm excited to hear from you and explore how we can work together to bring your ideas to life.
+                    </h3>
                 </div>
-                </Fade>
-                <Fade direction="right" duration={1500}>
-                <div className=' card w-80 md:w-96 p-4 hover:bg-base-300'>
-                    <h3 className="text-xl text-center">Email Us</h3>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Name</span>
-                        </label>
-                        <input type="name" placeholder="email" className="input input-bordered" />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Email</span>
-                        </label>
-                        <input type="email" placeholder="email" className="input input-bordered" />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Message</span>
-                        </label>
-                        <textarea className="textarea textarea-bordered h-24" placeholder="Your message is here"></textarea>
-                    </div>
-                    <div className="form-control mt-6">
-                        <button className="btn btn-outline">Send</button>
-                    </div>
+                <div data-aos="fade-up"
+                    data-aos-duration="2000" className="hero-content flex-col lg:flex-row">
+                    <div data-aos="fade-right" className="w-full max-w-xl p-5 rounded-xl ">
+                        
+                        <div className='sm:p-10 md:p-0 lg:p-2 '>
+                            <Lottie animationData={contact} loop={true} className='h-[650px]' />
+                        </div>
 
+                        {/* <Fade direction='left' className=' ps-28'>
+                        <div className=''>
+                            <h2 className="text-xl shadow-none">Phone</h2>
+                            <figure style={{ justifyContent: 'flex-start' }} className='flex items-center' >
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-18 w-12" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fillRule="evenodd" d="M7 2a2 2 0 00-2 2v12a2 2 0 002 2h6a2 2 0 002-2V4a2 2 0 00-2-2H7zm3 14a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
+                                </svg>
+                                <a href="tel:+8801772893112">+8801772893112</a>
+                            </figure>
+                        </div>
+                        <div className='my-2'>
+                            <h2 className="text-xl">Email</h2>
+                            <figure style={{ justifyContent: 'flex-start' }} className='flex items-center' >
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-18 w-12" viewBox="0 0 20 20" fill="currentColor">
+                                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+                                </svg>
+                                <a href="mailto:alomgirhossainrocky@gmail.com">alomgirhossainrocky@gmail.com</a>
+                            </figure>
+                        </div>
+                         <div>
+                            <h2 className="text-2xl">Address</h2>
+                            <figure style={{ justifyContent: 'flex-start' }} className='flex items-center' >
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-18 w-12" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
+                                </svg>
+                                <address>Kajla, Matihar, Rajshahi </address>
+                            </figure>
+                        </div> 
+                        </Fade> */}
+                    </div>
+                    <Fade direction='right'>
+                    <div data-aos="fade-left" className="card flex-shrink-0 w-full max-w-lg shadow-xl">
+                        <div className="card-body">
+                            <h3 className="text-md text-center text-primary underline underline-offset-2">Direct Mail</h3>
+                            <form ref={form} onSubmit={handleSubmit}>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Name</span>
+                                    </label>
+                                    <input required type="name" name='user_name' placeholder="Name" className="input input-bordered" />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input required type="email" name='user_email' placeholder="Email" className="input input-bordered" />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Message</span>
+                                    </label>
+                                    <textarea required type="text" name='message' rows={5} cols={30} className="textarea textarea-bordered" placeholder="Message"></textarea>
+                                </div>
+                                <div className="form-control mt-6">
+                                    <input type='submit' value='Send' className="btn bg-yellow-500 border-0 hover:bg-green-500"></input>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    </Fade>
                 </div>
-                </Fade>
             </div>
-
         </div>
     );
 };
