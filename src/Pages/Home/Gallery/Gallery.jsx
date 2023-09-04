33811050a84d37b13efa06f14e98ecd67721d035
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-09-05 01:22:50 +0600

    added Gallery section

---
+++ src/Pages/Home/Gallery/Gallery.jsx
@@ -0,0 +1,30 @@
+import pic1 from '../../../assets/images/Forest.jpg'
+import pic2 from '../../../assets/images/hiking.jpg'
+import pic3 from '../../../assets/images/image4.jpeg'
+import pic4 from '../../../assets/images/img-2.webp'
+import pic5 from '../../../assets/images/img-4.webp'
+import pic6 from '../../../assets/images/keyekingjpg.jpg'
+import pic7 from '../../../assets/images/Mountain.jpeg'
+import pic8 from '../../../assets/images/img-3.webp'
+
+const Gallery = () => {
+    return (
+        <div>
+            <div className="my-20 mx-10">
+           
+            <div className=" relative  grid lg:grid-cols-4 sm:grid-cols-1 gap-7 justify-between"> 
+            <img className="transition-transform duration-300 w-full lg:w-64 lg:h-64 hover:scale-110 rounded-lg" src={pic1} alt="" />
+            <img className="transition-transform duration-300 w-full lg:w-64 lg:h-64 hover:scale-110 rounded-lg" src={pic2} alt="" />
+            <img className="transition-transform duration-300 w-full lg:w-64 lg:h-64 hover:scale-110 rounded-lg" src={pic3} alt="" />
+            <img className="transition-transform duration-300 w-full lg:w-64 lg:h-64 hover:scale-110 rounded-lg" src={pic4} alt="" />
+            <img className="transition-transform duration-300 w-full lg:w-64 lg:h-64 hover:scale-110 rounded-lg" src={pic5} alt="" />
+            <img className="transition-transform duration-300 w-full lg:w-64 lg:h-64 hover:scale-110 rounded-lg" src={pic6} alt="" />
+            <img className="transition-transform duration-300 w-full lg:w-64 lg:h-64 hover:scale-110 rounded-lg" src={pic7} alt="" />
+            <img className="transition-transform duration-300 w-full lg:w-64 lg:h-64 hover:scale-110 rounded-lg" src={pic8} alt="" />
+        </div>
+        </div>
+        </div>
+    );
+};
+
+export default Gallery;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 // import Banner from "../Banner/Banner";
 import Bannert from "../Banner/Bannert";
 import Contact from "../Contact/Contact";
+import Gallery from "../Gallery/Gallery";
 import GuideLine from "../GuideLine/GuideLine";
 import InstractorsPage from "../InstractorsPage/InstractorsPage";
 import PopularClasses from "../PopularClasses/PopularClasses";
@@ -15,6 +16,7 @@ const Home = () => {
             <PopularClasses></PopularClasses>
             <InstractorsPage></InstractorsPage>
             <GuideLine></GuideLine>
+            <Gallery></Gallery>
             <Contact></Contact>
         </div>
     );
